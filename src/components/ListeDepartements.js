7edf426ac6af2c10ed740f8698a8addec84fa9cf
aurélien <achatelain62000@gmail.com>
2021-08-03 20:42:28 +0200

    ajout api place

--- src/components/ListeDepartements.js
+++ src/components/ListeDepartements.js
@@ -3,13 +3,12 @@ import { withRouter } from "react-router";
 
 import axios from "axios";
 import querystring from "query-string";
-import Autocomplete from "react-google-autocomplete";
-import {
-  Select,
-  InputLabel,
-  MenuItem,
-  Button,
-} from "@material-ui/core";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+
+import { Select, InputLabel, MenuItem, Button, Input } from "@material-ui/core";
 
 class ListDepartements extends Component {
   constructor(props) {
@@ -17,6 +16,7 @@ class ListDepartements extends Component {
     this.state = {
       token: null,
       departements: [],
+      address: "",
     };
   }
 
@@ -80,6 +80,17 @@ class ListDepartements extends Component {
     this.props.history.push("/listeResults");
   };
 
+  handleChange = (address) => {
+    this.setState({ address });
+  };
+
+  handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log("Success", latLng))
+      .catch((error) => console.error("Error", error));
+  };
+
   render() {
     const { departements } = this.state;
     const { codeDep } = this.props.codeDep;
@@ -105,13 +116,49 @@ class ListDepartements extends Component {
           </Select>
           <br />
           <br />
-          <Autocomplete
-            apiKey="3AIzaSyBxYWqRQwP60wOhjyE6kXVDM3GpaVLi_ro"
-            onPlaceSelected={(place) => {
-              console.log(place);
-            }}
-          />
-          ;
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading,
+            }) => (
+              <div>
+                <Input
+                  {...getInputProps({
+                    placeholder: "Search Places ...",
+                    className: "location-search-input",
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                      : { backgroundColor: "#ffffff", cursor: "pointer" };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
           <br />
           <br />
           <Button
