d50487b87d2091771144ed7008c34ad0a3bd24ce
aurélien <achatelain62000@gmail.com>
2021-08-05 22:17:02 +0200

    Modification requete insee

--- src/components/ListResults.js
+++ src/components/ListResults.js
@@ -12,22 +12,76 @@ class ListResults extends Component {
     const date =
       today.getFullYear() +
       "-" +
-      ((today.getMonth() + 1)<10?"0"+(today.getMonth() + 1) : (today.getMonth() + 1)) +
+      (today.getMonth() + 1 < 10
+        ? "0" + (today.getMonth() + 1)
+        : today.getMonth() + 1) +
       "-" +
-      today.getDate();
+      (today.getDate() + 1 < 10
+        ? "0" + (today.getDate() + 1)
+        : today.getDate() + 1);
 
     this.state = {
       token: null,
       etablissements: [],
-      header: {},
       date: date,
-      nombre: 1000,
+      nombreResultRequeteTotal: 0,
+      nombreDebutRequete: 0,
+      nombreResultRequete: 1000,
+      typeVoie: [
+        { court: "ALL", long: "Allée" },
+        { court: "AV", long: "Avenue" },
+        { court: "BD", long: "Boulevard" },
+        { court: "CAR", long: "Carrefour" },
+        { court: "CHE", long: "Chemin" },
+        { court: "CHS", long: "Chaussée" },
+        { court: "CITE", long: "Cité" },
+        { court: "COR", long: "Corniche" },
+        { court: "CRS", long: "Cours" },
+        { court: "DOM", long: "Domaine" },
+        { court: "DSC", long: "Descente" },
+        { court: "ECA", long: "Ecart" },
+        { court: "ESP", long: "Esplanade" },
+        { court: "FG", long: "Faubourg" },
+        { court: "GR", long: "Grande Rue" },
+        { court: "HAM", long: "Hameau" },
+        { court: "HLE", long: "Halle" },
+        { court: "IMP", long: "Impasse" },
+        { court: "LD", long: "Lieu-dit" },
+        { court: "LOT", long: "Lotissement" },
+        { court: "MAR", long: "Marché" },
+        { court: "MTE", long: "Montée" },
+        { court: "PAS", long: "Passage" },
+        { court: "PL", long: "Place" },
+        { court: "PLN", long: "Plaine" },
+        { court: "PLT", long: "Plateau" },
+        { court: "PRO", long: "Promenade" },
+        { court: "PRV", long: "Parvis" },
+        { court: "QUA", long: "Quartier" },
+        { court: "QUAI", long: "Quai" },
+        { court: "RES", long: "Résidence" },
+        { court: "RLE", long: "Ruelle" },
+        { court: "ROC", long: "Rocade" },
+        { court: "RPT", long: "Rond-point" },
+        { court: "RTE", long: "Route" },
+        { court: "RUE", long: "Rue" },
+        { court: "SEN", long: "Sente - Sentier" },
+        { court: "SQ", long: "Square" },
+        { court: "TPL", long: "Terre-plein" },
+        { court: "TRA", long: "Traverse" },
+        { court: "VLA", long: "Villa" },
+        { court: "VLGE", long: "Village" },
+      ],
     };
   }
 
   componentDidMount() {
     const { codeDep, codeNaf } = this.props;
-    const { date, nombre } = this.state;
+    const {
+      date,
+      nombreResultRequete,
+      nombreDebutRequete,
+      nombreResultRequeteTotal,
+    } = this.state;
     const data = {
       grant_type: "client_credentials",
       validity_period: 604800,
@@ -48,38 +102,56 @@ class ListResults extends Component {
           token: token,
         });
 
-        const sirene = axios.create({
-          baseURL: "https://api.insee.fr/entreprises/sirene/V3",
-        });
-        sirene.defaults.headers.common["Authorization"] =
-          "Bearer " + this.state.token;
-        sirene.defaults.headers.common["Accept"] = "application/json";
-        const params = {
-          params: {
-            q:
-              "codePostalEtablissement:[" +
-              codeDep +
-              "000 TO " +
-              codeDep +
-              "999] AND activitePrincipaleUniteLegale:" +
-              codeNaf +
-              " AND etatAdministratifUniteLegale:A",
-            date: date,
-            nombre: nombre,
-          },
-        };
-        sirene
-          .get("/siret", params)
-          .then((response) => {
-            this.setState({
-              etablissements: response.data.etablissements,
-              header: response.data.header,
-            });
-            console.log(response);
-          })
-          .catch((error) => {
-            console.log(error);
+        do {
+          console.log("je boucle");
+          const sirene = axios.create({
+            baseURL: "https://api.insee.fr/entreprises/sirene/V3",
           });
+          sirene.defaults.headers.common["Authorization"] =
+            "Bearer " + this.state.token;
+          sirene.defaults.headers.common["Accept"] = "application/json";
+          const params = {
+            params: {
+              q:
+                "activitePrincipaleUniteLegale:" +
+                codeNaf +
+                " AND etatAdministratifUniteLegale:A",
+              date: date,
+              tri: "siret",
+              debut: nombreDebutRequete,
+              nombre: nombreResultRequete,
+              champs:
+                "siret,denominationUniteLegale,numeroVoieEtablissement,typeVoieEtablissement,libelleVoieEtablissement,codePostalEtablissement,libelleCommuneEtablissement",
+            },
+          };
+          sirene
+            .get("/siret", params)
+            .then((response) => {
+              const tmpEtablissements = this.state.etablissements.concat(
+                response.data.etablissements
+              );
+              console.log(response.data.header.total);
+              this.setState({
+                etablissements: tmpEtablissements,
+                nombreResultRequeteTotal: response.data.header.total,
+                nombreDebutRequete:
+                  this.state.nombreDebutRequete + nombreResultRequete,
+              });
+
+              console.log(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          console.log(
+            "test : nb: " +
+              nombreDebutRequete +
+              " < total: " +
+              nombreResultRequeteTotal +
+              " => " +
+              (nombreDebutRequete < nombreResultRequeteTotal)
+          );
+        } while (nombreDebutRequete < nombreResultRequeteTotal);
       })
       .catch((error) => {
         console.log(error);
@@ -87,26 +159,32 @@ class ListResults extends Component {
   }
 
   render() {
-    const { token, etablissements, header } = this.state;
+    const { token, etablissements, nombreResultRequeteTotal } = this.state;
     const { codeDep, codeNaf } = this.props;
     var id = 0;
-    const listEtablissements = etablissements.map((etablissement) => {
-      if (etablissement.uniteLegale.nomUniteLegale != null) {
+    /*const listEtablissements = etablissements.map((etablissement) => {
+      if (etablissement.uniteLegale.denominationUniteLegale != null) {
         id++;
-        return <p key={id}>{etablissement.uniteLegale.nomUniteLegale}</p>;
+        return (
+          <p key={id}>{etablissement.uniteLegale.denominationUniteLegale}</p>
+        );
       } else {
         id++;
         return <p key={id}>Sans nom</p>;
       }
-    });
+        <p>{listEtablissements}</p>
+    });*/
 
     return (
       <Fragment>
-        <p>{header.total}</p>
+        <p>{nombreResultRequeteTotal}</p>
         <p>{codeNaf}</p>
         <p>{codeDep}</p>
         <p>{token}</p>
-        <p>{listEtablissements}</p>
+        <p>{etablissements.length}</p>
+        {etablissements.map((etablissement, index) => {
+          return <p key={index}>{index}</p>;
+        })}
       </Fragment>
     );
   }
